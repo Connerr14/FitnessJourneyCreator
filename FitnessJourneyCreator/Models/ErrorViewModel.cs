ef4482875da8a690913d0948948c23c7ada3f938
Connerr14 <conner7recoskie@gmail.com>
2024-09-18 10:38:26 -0400

    Add project files.

---
+++ FitnessJourneyCreator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessJourneyCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
