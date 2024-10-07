6820050e32ffdfb326ddc9fcd17a52910bc2a86c
Connerr14 <conner7recoskie@gmail.com>
2024-10-07 15:17:07 -0400

    Prepared for migration by adding DB Sets.

--- FitnessJourneyCreator/Data/ApplicationDbContext.cs
+++ FitnessJourneyCreator/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FitnessJourneyCreator.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitnessJourneyCreator.Data
@@ -9,5 +10,15 @@ namespace FitnessJourneyCreator.Data
             : base(options)
         {
         }
+        // Declaration of my DB Sets
+        public DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+        public DbSet<Macrocycle> Macrocycles { get; set; }
+        public DbSet<Mesocycle> Mesocycles { get; set; }
+        public DbSet<Microcycle> Microcycles { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
     }
 }
